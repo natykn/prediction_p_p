82907b13523fd64d7949d19f44a22a713dbccdb0
natykn <natyco16@gmail.com>
2017-10-16 02:17:52 -0500

    phpunit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,27 +13,43 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testConectionTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
 
-     public function picoPlacaMain()
+     public function testPicoPlacaMain()
     {
         $response = $this->get('/picoplacapredictor');
 
         $response->assertStatus(200);
     }
 
-    // public function lastPlateDigit(){
-    //     $picoPlacaObj = new PicoPlacaPredictorModel("PBU-1378","2017-10-05","9:00");
-    //     $this->assertEquals(6,$picoPlacaObj->getLastDigitFromPlate("PCU-9856"));
-    // }
+    public function testLastPlateDigit(){
+        $picoPlacaObj = new PicoPlacaPredictorModel("PBU-1378","2017-10-05","9:00");
+        $this->assertEquals(8,$picoPlacaObj->getLastDigitFromPlate());
+    }
 
-    // public function dayNameFromDate(){
-    //     $picoPlacaObj = new PicoPlacaPredictorModel("PBU-1378","2017-10-16","9:00");
-    //     $this->assertEquals("Monday",$picoPlacaObj->getDayNameFromDate());
-    // }
+    public function testDayNameFromDate(){
+        $picoPlacaObj = new PicoPlacaPredictorModel("PBU-1378","2017-10-16","9:00");
+        $this->assertEquals("Monday",$picoPlacaObj->getDayNameFromDate());
+    }
+
+    public function testNotInPicoPlaca(){
+        $picoPlacaObj = new PicoPlacaPredictorModel("PCT-1376","2017-10-04","2:00");
+        $this->assertEquals(false,$picoPlacaObj->predictPicoPlaca());
+    }
+
+    public function testInPicoPlaca(){
+        $picoPlacaObj = new PicoPlacaPredictorModel("PCT-1375","2017-09-13","16:00");
+        $this->assertEquals(true,$picoPlacaObj->predictPicoPlaca());
+    }
+
+    public function testPredictionText(){
+        $picoPlacaObj = new PicoPlacaPredictorModel("ABC-0070","2017-07-07","08:20");
+        $result=$picoPlacaObj->predictPicoPlaca();
+        $this->assertContains("not",$picoPlacaObj->setPredictionText($result));
+    }
 }
